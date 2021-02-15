f53b2344d4e500b0a9f53e8447162b0784ec267b
Johannes L <johannes.dominic@gmail.com>
2021-02-14 22:38:27 -0600

    worked on login backend validation

--- hexafuel_oil/hexafuel_oil_app/views.py
+++ hexafuel_oil/hexafuel_oil_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
+
+    print("REQUEST", request.POST)
+    username = str(request.POST.get('username'))
+    pwd = str(request.POST.get('password'))
+    
+    print(type(username)) #-> <class 'str'>
+    print(type(pwd)) #-> <class 'str'>
+    
+    if (username == "") or (len(username) > 10):
+        return JsonResponse({"ValidationError": "username cannot be empty or cannot exceed 10 chars."})
+
+    if pwd == "":
+        return JsonResponse({"ValidationError": "password cannot be empty."})
+
     return render(request, 'hexafuel_oil_app/login.html')
 
 def form(request):
