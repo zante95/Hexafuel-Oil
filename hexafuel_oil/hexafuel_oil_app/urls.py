a001965c1d6cbfff703764004aad47948a81ab5a
Johannes L <johannes.dominic@gmail.com>
2021-03-04 21:40:47 -0600

    worked on converting function based views into class based views

--- hexafuel_oil/hexafuel_oil_app/urls.py
+++ hexafuel_oil/hexafuel_oil_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import HomeView, RegisterView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('form/', views.form, name="form"),
-    path('register/', views.register, name="register"),
+    path('register/', RegisterView.as_view(), name="register"),
     path('history/', views.history, name="history"),
     path('profile/', views.profile, name="profile"),
 ]
